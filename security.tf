f5b5fb03be9a038b26633b580f420bc2e09d72a6
Himal Pandey <pandu.himal@gmail.com>
2026-07-25 19:57:22 +1000

    fix restrict sg egress to 443 for SSM connectivity

--- security.tf
+++ security.tf
@@ -9,11 +9,4 @@ resource "aws_security_group" "public_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
