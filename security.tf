bac3b74d48f0a62ae2daf37d318ee9eb5b2986bd
Himal Pandey <pandu.himal@gmail.com>
2026-07-25 17:06:10 +1000

    configure public security group for secure SSM access

---
+++ security.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "public_sg" {
+  name        = "ssm-public-sg"
+  description = "Allow outbound traffic for SSM agent"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
