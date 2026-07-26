fa7712fbda75da97b888ba17c461648b988c7946
Himal Pandey <pandu.himal@gmail.com>
2026-07-26 17:38:20 +1000

    add private ec2 instance and security group

--- main.tf
+++ main.tf
@@ -16,3 +16,22 @@ resource "aws_instance" "public_instance" {
     Name = "public_instance"
   }
 }
+
+resource "aws_instance" "private_instance" {
+  ami                         = data.aws_ami.linux.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnet.id
+  vpc_security_group_ids      = [aws_security_group.private_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
+  associate_public_ip_address = false
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  tags = {
+    Name = "private_instance"
+  }
+}
--- security.tf
+++ security.tf
@@ -10,3 +10,16 @@ resource "aws_security_group" "public_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "private_sg" {
+  name        = "ssm-private-sg"
+  description = "Private subnet instance reaching SSM via NAT Gateway (no direct IGW route)"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
